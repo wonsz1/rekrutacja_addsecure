a7a44fe5b37e1e3f1193af0e6e95c651bf9dcf66
Mateusz Rusiecki <mateuszr87@gmail.com>
2025-09-14 21:24:10 +0200

    delete unused code

--- src/Domain/Service/VehiclesReader.php
+++ src/Domain/Service/VehiclesReader.php
@@ -12,29 +12,8 @@ class VehiclesReader
     ) {
     }
 
-    public function getVehicleById(int $id): ?VehicleDTO
+    public function getVehicleById(int $id): ?Vehicle
     {
-        $vehicle = $this->vehicleRepository->getById($id);
-        
-        if (!$vehicle) {
-            return null;
-        }
-        
-        return $this->entityToDTO($vehicle);
-    }
-
-
-    private function entityToDTO(Vehicle $vehicle)
-    {
-        $vehicleDTO = new VehicleDTO();
-        $vehicleDTO->id = $vehicle->getId();
-        $vehicleDTO->registrationNumber = $vehicle->getRegistrationNumber();
-        $vehicleDTO->brand = $vehicle->getBrand();
-        $vehicleDTO->model = $vehicle->getModel();
-        $vehicleDTO->type = $vehicle->getType();
-        $vehicleDTO->createdAt = $vehicle->getCreatedAt();
-        $vehicleDTO->updatedAt = $vehicle->getUpdatedAt();
-
-        return $vehicleDTO;
+        return $this->vehicleRepository->getById($id);
     }
 }
